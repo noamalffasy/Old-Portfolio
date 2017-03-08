34dc3e941a1ca629a41395ada5bc171a290d4547
Noam Alffasy <noahm1234@outlook.com>
2017-03-08 19:13:35 +0200

    Utilize for touch screen devices

--- main.bundle.js
+++ main.bundle.js
@@ -350,14 +350,14 @@ var ItemComponent = (function () {
     }
     ItemComponent.prototype.ngOnInit = function () {
         this.calcHeight();
-        $('a.taphover').on('touchstart', function (e) {
+        $('.visit').on('touchstart', function (e) {
             var link = $(this); //preselect the link
             if (link.hasClass('hover')) {
                 return true;
             }
             else {
                 link.addClass('hover');
-                $('a.taphover').not(this).removeClass('hover');
+                $('.visit').not(this).removeClass('hover');
                 e.preventDefault();
                 return false; //extra, and to make sure the function has consistent return points
             }
